35fbd1029eece6b549a540df69efd6def02b7076
Hendrawan <wanzy3901@gmail.com>
2026-09-15 17:48:04 +0700

    feat: add owner dashboard route

---
+++ app/owner/page.tsx
@@ -0,0 +1,5 @@
+import OwnerDashboard from "@/components/Owner/OwnerDashboard";
+
+export default function OwnerPage() {
+  return <div className="min-h-screen bg-white text-[#111] dark:bg-[#0B0B0C] dark:text-white"><OwnerDashboard /></div>;
+}
