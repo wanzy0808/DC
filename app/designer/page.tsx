ab389e37747d14dd05a6d59ed0eb7cab75feba52
Hendrawan <wanzy3901@gmail.com>
2026-09-15 17:50:03 +0700

    feat: add designer dashboard route

---
+++ app/designer/page.tsx
@@ -0,0 +1,3 @@
+import DesignerDashboard from "@/components/Designer/DesignerDashboard";
+
+export default function DesignerPage(){return <div className="min-h-screen bg-white text-[#111] dark:bg-[#0B0B0C] dark:text-white"><DesignerDashboard/></div>}
