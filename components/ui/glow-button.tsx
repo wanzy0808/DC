8c273a2f5a11e724b9cb8410be9cea8136bba693
wanzy0808 <wanzy3901@gmail.com>
2026-09-15 12:57:07 +0700

    perubahan minor

--- components/ui/glow-button.tsx
+++
@@ -1,9 +0,0 @@
-// Deprecated compatibility re-export.
-// Button is the single canonical application button primitive.
-export {
-  Button as GlowButton,
-  buttonVariants as glowButtonVariants,
-  type ButtonProps as GlowButtonProps,
-} from "@/components/ui/button"
-
-export { Button as Component } from "@/components/ui/button"
