0ab294e29b92c97afcdd2104e6e3337f57087430
Hendrawan <wanzy3901@gmail.com>
2026-09-20 08:13:55 +0700

    Type Motion orbital controls for Three engine

--- app/jiplak/three-portal-engine.d.ts
+++ app/jiplak/three-portal-engine.d.ts
@@ -3,6 +3,9 @@ export type PortalId = 1 | 2 | 3;
 export type PortalEngineOptions = {
   getActiveDoor: () => PortalId;
   getDarkMode: () => boolean;
+  getOrbit: () => number;
+  onHover: (door: PortalId) => void;
+  onLeave: () => void;
   onChoose: (door: PortalId) => void;
   onEnter: (door: PortalId) => void;
   reducedMotion: boolean;
