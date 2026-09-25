99880cf23061eb583f7a2a76b198e6ef49d3e0e0
Hendrawan <wanzy3901@gmail.com>
2026-09-25 15:32:32 +0700

    Studio: add Designer entry to shared Template Studio

---
+++ app/designer/studio/page.tsx
@@ -0,0 +1,5 @@
+import InvitationEditorPage from "@/components/InvitationStudio/InvitationEditorPage";
+
+export default function DesignerTemplateStudioPage() {
+  return <InvitationEditorPage mode="template" backHref="/designer" />;
+}
