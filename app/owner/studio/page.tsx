5c1f8e9560fb05476d72593694150bdec78c05d2
Hendrawan <wanzy3901@gmail.com>
2026-09-25 15:32:36 +0700

    Studio: add Owner entry to shared Template Studio

---
+++ app/owner/studio/page.tsx
@@ -0,0 +1,5 @@
+import InvitationEditorPage from "@/components/InvitationStudio/InvitationEditorPage";
+
+export default function OwnerTemplateStudioPage() {
+  return <InvitationEditorPage mode="template" backHref="/owner" />;
+}
